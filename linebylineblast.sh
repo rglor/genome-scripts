bcbb4355efbcfa75c8bbc0fd3dddac88bf735dee
Alana Alexander <laninsky@gmail.com>
2016-08-10 09:28:41 -0500

    Update linebylineblast.sh

--- linebylineblast.sh
+++ linebylineblast.sh
@@ -4,5 +4,12 @@
 ~/bin/ncbi-blast-2.2.30+/bin/makeblastdb -in a.lines.fasta -dbtype nucl
 
 #Step 3: line by line blast. Make sure that linebyline.R is in the directory with your fasta file and tempout file.
-for i in `seq 1 $nosamples`;
-do name=`tail -n+$i samplenames.txt | head -n1`;
+noseqs=`wc -l tempout | awk '{print $1}'`
+for i in `seq 1 2 $noseqs`;
+do tail -n+$i tempout | head -n1 > tempseq;
+j=`expr $i + 1`;
+tail -n+$j tempout | head -n1 >> tempseq;
+tail -n+$j tempout | head -n1 | awk '{print length}' > length.txt;
+~/bin/ncbi-blast-2.2.30+/bin/blastn -db a.lines.fasta -query tempseq -perc_identity 75 -outfmt 6 > rawblast.txt;
+Rscript linebyline.R;
+done;
