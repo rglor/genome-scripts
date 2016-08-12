133df0b3327d380a1ee866b95dfbbc7bdb60ed00
Alana Alexander <laninsky@gmail.com>
2016-08-12 16:54:12 -0500

    Update linebyline.R

--- linebyline.R
+++ linebyline.R
@@ -1,8 +1,11 @@
 length <- as.matrix(read.table("length.txt"))
 blast <- as.matrix(read.table("rawblast.txt"))
+if(dim(blast)[1]>1) {
+blast <- blast[-1,]
 blast <- blast[(which(blast[,1]==blast[,2])),]
-blast <- blast[(which(blast[,7]!=blast[,9])),]
+blast <- blast[(which(as.numeric(blast[,7])!=as.numeric(blast[,9]))),]
 length <- matrix(length,ncol=1,nrow=(dim(blast)[1]))
 blast <- cbind(blast,length)
 blast <- blast[,-1]
 write.table(blast, "blastoutput.txt",quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
+}
