29d091b2ee6cbb676b5569692cfcd5bec4fbcb25
Alana Alexander <laninsky@gmail.com>
2016-08-21 15:58:59 -0500

    Update linebyline.R

--- linebyline.R
+++ linebyline.R
@@ -6,10 +6,21 @@ blast <- blast[-1,]
 blast <- blast[(which(blast[,1]==blast[,2])),]
 if(is.matrix(blast)) {
 blast <- blast[(which(as.numeric(blast[,7])!=as.numeric(blast[,9]))),]
+if(is.matrix(blast)) {
 length <- matrix(length,ncol=1,nrow=(dim(blast)[1]))
 blast <- cbind(blast,length)
 blast <- blast[,-1]
 write.table(blast, "blastoutput.txt",quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
+} else {
+if(!(blast[7]==blast[9])) {
+blast <- t(as.matrix(blast))
+length <- matrix(length,ncol=1,nrow=(dim(blast)[1]))
+blast <- cbind(blast,length)
+blast <- blast[,-1]
+blast <- t(as.matrix(blast))
+write.table(blast, "blastoutput.txt",quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
+}
+}
 } else {
 if(!(blast[7]==blast[9])) {
 blast <- t(as.matrix(blast))
