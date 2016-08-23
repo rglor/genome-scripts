f3430d69ae848f88ddb904235077b4eaf53bd63e
Alana Alexander <laninsky@gmail.com>
2016-08-23 14:52:35 -0500

    Update linebylineblast.sh

--- linebylineblast.sh
+++ linebylineblast.sh
@@ -8,9 +8,12 @@
 noseqs=`wc -l tempout | awk '{print $1}'`
 echo "seqid" "pident" "match_length" "mismatch" "gapopen" "qstart" "qend" "sstart" "send" "evalue" "bitscore" "scaffold_length" > blastoutput.txt
 for i in `seq 1 2 $noseqs`;
-do tail -n+$i tempout | head -n1 > tempseq;
+do tail -n+$i tempout | head -n1 > tempinseq;
 j=`expr $i + 1`;
-tail -n+$j tempout | head -n1 >> tempseq;
+tail -n+$j tempout | head -n1 >> tempinseq;
+~/bin/ncbi-blast-2.2.30+/bin/dustmasker -in tempinseq -out tempoutseq -outfmt fasta;
+Rscript onelining_tempseq.R;
+rm tempoutseq;
 tail -n+$j tempout | head -n1 | awk '{print length}' > length.txt;
 echo "qseqid" "sqeqid" "pident" "match_length" "mismatch" "gapopen" "qstart" "qend" "sstart" "send" "evalue" "bitscore" > rawblast.txt;
 ~/bin/ncbi-blast-2.2.30+/bin/blastn -db a.lines.fasta -query tempseq -perc_identity 75 -outfmt 6 >> rawblast.txt;
