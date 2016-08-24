4dd3874b003b5437210915c3fcc4868ae330ec5b
Alana Alexander <laninsky@gmail.com>
2016-08-24 09:14:49 -0500

    Update linebylineblast.sh

--- linebylineblast.sh
+++ linebylineblast.sh
@@ -1,9 +1,6 @@
 #Step 1: run the onelining.R script on your original genome fasta file
  
-#Step 2: make the blast database. Change the file name following -in to whatever your original fasta file was called. You'll need ot change it in line 19 too.
-~/bin/ncbi-blast-2.2.30+/bin/makeblastdb -in trunk_anole_19Jun2016_xkeD9.fasta -dbtype nucl
-
-#Step 3: line by line blast. Make sure that linebyline.R is in the directory with your fasta file and tempout file.
+#Step 2: line by line blast to itself. Make sure that linebyline.R is in the directory with your fasta file and tempout file.
 #Change db to whatever yours is called in line 19
 noseqs=`wc -l tempout | awk '{print $1}'`
 echo "seqid" "pident" "match_length" "mismatch" "gapopen" "qstart" "qend" "sstart" "send" "evalue" "bitscore" "scaffold_length" > blastoutput.txt
@@ -16,6 +13,7 @@ Rscript onelining_tempseq.R;
 rm tempoutseq;
 tail -n+2 tempseq | head -n1 | awk '{print length}' > length.txt;
 echo "qseqid" "sqeqid" "pident" "match_length" "mismatch" "gapopen" "qstart" "qend" "sstart" "send" "evalue" "bitscore" > rawblast.txt;
-~/bin/ncbi-blast-2.2.30+/bin/blastn -db trunk_anole_19Jun2016_xkeD9.fasta -query tempseq -perc_identity 75 -outfmt 6 -lcase_masking >> rawblast.txt;
+~/bin/ncbi-blast-2.2.30+/bin/makeblastdb -in tempseq -dbtype nucl;
+~/bin/ncbi-blast-2.2.30+/bin/blastn -db tempseq -query tempseq -perc_identity 75 -outfmt 6 -lcase_masking >> rawblast.txt;
 Rscript linebyline.R >> linebylineblastlog.txt;
 done;
